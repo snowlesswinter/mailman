88b4a4cec3d0336f8f58ca234f07a67d4e070d6e
tanjianwen <jianwen.tan@gmail.com>
2018-05-10 15:06:54 +0800

    added mail translation

--- codec.py
+++ codec.py
@@ -1,6 +1,8 @@
 from Crypto.Cipher import AES
 
 import base64
+import json
+import pyperclip
 
 def get_nonce():
     return b'\xde\xad\xbe\xef'
@@ -12,11 +14,21 @@ def build_mail(ip, key):
 
 def translate_mail(mail, key):
     cipher = AES.new(key.encode('utf-8'), AES.MODE_EAX, nonce=get_nonce())
-    return cipher.decrypt(base64.b64decode(mail))
+    return cipher.decrypt(base64.b64decode(mail)).decode('utf-8')
+
+def load_config():
+    with open('config.txt', 'r') as config_file:
+        return json.loads(config_file.read(-1))
+
+    raise
 
 def main():
-    raw_mail = translate_mail('xxxx')
-    print('content: ', raw_mail)
+    config = load_config()
+    mail_content = input('Enter mail content: ')
+    raw_mail = translate_mail(mail_content, config['key'])
+    pyperclip.copy(raw_mail)
+    print('ip copied to clipboard: ', raw_mail)
+    input()
 
 if __name__ == '__main__':
     main()
